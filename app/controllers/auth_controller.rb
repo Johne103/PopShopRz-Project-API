a038c5e78a0cd0127a005b690be4a52f6163aaf5
John Evans <johne103@hotmail.com>
2018-02-26 22:41:59 +0000

    Remove is_store field from user_params

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -24,6 +24,6 @@ class AuthController < ApplicationController
 
   private
       def user_params
-        params.permit(:username, :email, :password, :password_confirmation, :is_store)
+        params.permit(:username, :email, :password, :password_confirmation)
       end
 end
