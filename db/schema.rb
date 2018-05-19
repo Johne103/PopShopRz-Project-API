e5e566e4b7c00d95b43b166b63a8a92375ae8a7c
John Evans <johne103@hotmail.com>
2018-05-19 17:07:09 +0100

    add products database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225190750) do
+ActiveRecord::Schema.define(version: 20180519153431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.text     "description"
+    t.string   "image"
+    t.float    "original_price"
+    t.float    "offer_price"
+    t.string   "end_time"
+    t.boolean  "is_available"
+    t.integer  "quantity"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["user_id"], name: "index_products_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -34,4 +49,5 @@ ActiveRecord::Schema.define(version: 20180225190750) do
     t.datetime "updated_at",            null: false
   end
 
+  add_foreign_key "products", "users"
 end
